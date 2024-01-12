7dd50fba8c45829945516e1adae5c7cfe71edc89
ahmadtomeh03 <tomehahmad03@gmail.com>
2024-01-12 17:39:36 +0200

    ahmad

---
+++ src/company_project/employee.java
@@ -0,0 +1,31 @@
+package company_project;
+public class employee {
+    String name;// الاسم 
+    String numberofcard;// رقم الهوية
+    Integer salary;//المبلغ 
+    String numberofphone;// رقم الهوية  
+    public String getName() {
+        return name;
+    }
+    public String getNumberofcard() {
+        return numberofcard;
+    }
+    public Integer getSalary() {
+        return salary;
+    }
+    public String getNumberofphone() {
+        return numberofphone;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setNumberofcard(String numberofcard) {
+        this.numberofcard = numberofcard;
+    }
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+    public void setNumberofphone(String numberofphone) {
+        this.numberofphone = numberofphone;
+    }
+}
